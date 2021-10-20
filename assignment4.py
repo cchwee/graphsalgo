0a4bbba191b152cb9bbcb9fa9733b45155b741fa
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-10-21 01:16:29 +0800

    creating graph with edges done

--- assignment4.py
+++ assignment4.py
@@ -1,36 +1,69 @@
-class WordGraph:
+def string_comparison(word1, word2) -> bool:
+    differ = False
+    i = 0
+    while i in range(len(word1)):
+        if word1[i] == word2[i]:
+            i += 1
+            continue
+        else:
+            if differ == True:
+                return False
+            else:
+                i += 1
+                differ = True
+    return differ
 
-    # take as input a nonempty list of words and construct
-    # an appropriate graph from them. The words will contain only lowercase a-z characters, and
-    # will all be the same length, which will not be 0.
+class WordGraph:
     def __init__(self, V) -> None:
         self.vertices = [None] * len(V)
+
+        # creates all vertices in list
         for i in range(len(V)):
             self.vertices[i] = Vertex(V[i])
+        
+        # check whether to add edges - differ by 1 char
+        for i in range(len(V)):
+            for j in range(i+1, len(V)):
+                if string_comparison(V[i], V[j]):
+                    self.vertices[i].add_edge(i, j, 1)
+                    self.vertices[j].add_edge(j, i, 1)
 
     def __str__(self) -> str:
         return_str = ""
         for vertex in self.vertices:
             return_str = return_str + "Vertex " + str(vertex) + "\n"
         return return_str
-    
+
+
 class Vertex:
     def __init__(self, id) -> None:
         self.id = id 
         self.edges = []
     
+    def add_edge(self, source, destination, weight):
+        new_edge = Edge(source, destination, weight)
+        self.edges.append(new_edge)
+
     def __str__(self) -> str:
-        return_str = str(self.id)
-        return return_str
-    
+        return_string = str(self.id) + ", \n" + "my edges are: {"
+        for edge in self.edges:
+            return_string = return_string + str(edge)
+        return_string = return_string + "}"
+        return return_string
+
+
 class Edge:
     def __init__(self, u, v, w) -> None:
         self.u = u
         self.v = v
         self.w = w
+    
+    def __str__(self) -> str:
+        return_str = "[source: " + str(self.u) + ", " + "destination: " + str(self.v) + ", " + "weight: " + str(self.w) + "]"
+        return return_str
 
 if __name__ == "__main__":
-    vertices = ["aaa","aab","aac","aad","aae"]
+    vertices = ["aaa","aad","dad","daa","aca", "acc", "aab", "abb"]
     my_graph = WordGraph(vertices)
     print(my_graph)
 
