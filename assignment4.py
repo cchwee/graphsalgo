9e9a33ac584397834d393bddd9517623c2ce6d4b
Cassandra Chwee <cc.cassandrachwee@gmail.com>
2021-10-21 01:21:21 +0800

    rearrange positions of classes and functions

--- assignment4.py
+++ assignment4.py
@@ -1,3 +1,31 @@
+class Vertex:
+    def __init__(self, id) -> None:
+        self.id = id 
+        self.edges = []
+    
+    def add_edge(self, source, destination, weight):
+        new_edge = Edge(source, destination, weight)
+        self.edges.append(new_edge)
+
+    def __str__(self) -> str:
+        return_string = str(self.id) + ", \n" + "my edges are: {"
+        for edge in self.edges:
+            return_string = return_string + str(edge)
+        return_string = return_string + "}"
+        return return_string
+
+
+class Edge:
+    def __init__(self, u, v, w) -> None:
+        self.u = u
+        self.v = v
+        self.w = w
+    
+    def __str__(self) -> str:
+        return_str = "[source: " + str(self.u) + ", " + "destination: " + str(self.v) + ", " + "weight: " + str(self.w) + "]"
+        return return_str
+
+
 # string comparison between 2 words to see if they differ by 1 char
 def string_comparison(word1, word2) -> bool:
     differ = False
@@ -35,39 +63,15 @@ class WordGraph:
         for vertex in self.vertices:
             return_str = return_str + "Vertex " + str(vertex) + "\n"
         return return_str
-
-
-class Vertex:
-    def __init__(self, id) -> None:
-        self.id = id 
-        self.edges = []
     
-    def add_edge(self, source, destination, weight):
-        new_edge = Edge(source, destination, weight)
-        self.edges.append(new_edge)
-
-    def __str__(self) -> str:
-        return_string = str(self.id) + ", \n" + "my edges are: {"
-        for edge in self.edges:
-            return_string = return_string + str(edge)
-        return_string = return_string + "}"
-        return return_string
-
-
-class Edge:
-    def __init__(self, u, v, w) -> None:
-        self.u = u
-        self.v = v
-        self.w = w
-    
-    def __str__(self) -> str:
-        return_str = "[source: " + str(self.u) + ", " + "destination: " + str(self.v) + ", " + "weight: " + str(self.w) + "]"
-        return return_str
+    # Task 1
+    def best_start_word(self, target_words):
+        pass
 
 
 
 if __name__ == "__main__":
     vertices = ["aaa","aad","dad","daa","aca", "acc", "aab", "abb"]
     my_graph = WordGraph(vertices)
-    # print(my_graph)
+    print(my_graph)
 
